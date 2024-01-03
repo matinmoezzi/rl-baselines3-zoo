35b8cb2177cf4a01081b2829dca8afa01860b61e
Matin Moezzi <matin.moezzi@outlook.com>
2024-01-02 20:01:42 -0500

    Update train_mycobot.sh script

--- train_mycobot.sh
+++ train_mycobot.sh
@@ -1,1 +1,1 @@
-python train.py --algo $1 --env $2 -P --vec-env=subproc --device=cpu --log-interval 5000 --eval-freq 100000 --save-freq 100000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track --uuid
+python train.py --algo $1 --env $2 -P --vec-env=subproc --device=cpu --log-interval 5000 --eval-freq 100000 --save-freq 100000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track
