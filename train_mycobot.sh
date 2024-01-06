ea46649138b8d5168b0fbda6901ee04d206ec364
Matin Moezzi <matin.moezzi@outlook.com>
2024-01-06 16:25:40 -0500

    Update hyperparameters and training script

--- train_mycobot.sh
+++ train_mycobot.sh
@@ -1,1 +1,1 @@
-python train.py --algo $1 --env $2 -P --vec-env=subproc --device=cpu --log-interval 5000 --eval-freq 100000 --save-freq 100000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track
+python train.py --algo $1 --env $2 -P --vec-env=subproc --device=cpu --log-interval 500 --eval-freq 10000 --save-freq 100000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track
