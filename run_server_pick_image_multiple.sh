6a3258c78810928b496c5ffd22cf67c55f08fd7c
Matin Moezzi <matin.moezzi@outlook.com>
2024-01-13 10:18:32 -0500

    Update hyperparameters and training configurations

--- run_server_pick_image_multiple.sh
+++ run_server_pick_image_multiple.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 set -x
 
-bash train_mycobot.sh sac MyCobotFetchPickAndPlace-Dense-mocap-v1 --device cuda:4  --hyperparams gradient_steps:1 n_envs:10 learning_starts:10000 &
-bash train_mycobot.sh tqc MyCobotFetchPickAndPlace-Dense-mocap-v1 --device cuda:5  --hyperparams gradient_steps:1 n_envs:10 learning_starts:10000 &
+bash train_mycobot.sh sac MyCobotFetchPickAndPlace-Dense-mocap-v1 --device cuda:0  --hyperparams gradient_steps:1 n_envs:100 learning_starts:10000 &
+bash train_mycobot.sh tqc MyCobotFetchPickAndPlace-Dense-mocap-v1 --device cuda:0  --hyperparams gradient_steps:1 n_envs:100 learning_starts:10000 &
 
-bash train_mycobot.sh sac MyCobotFetchPickAndPlace-Sparse-mocap-v1 --device cuda:6  --hyperparams gradient_steps:1 n_envs:10 learning_starts:10000 &
-bash train_mycobot.sh tqc MyCobotFetchPickAndPlace-Sparse-mocap-v1 --device cuda:7  --hyperparams gradient_steps:1 n_envs:10 learning_starts:10000 &
+bash train_mycobot.sh sac MyCobotFetchPickAndPlace-Sparse-mocap-v1 --device cuda:1  --hyperparams gradient_steps:1 n_envs:100 learning_starts:10000 &
+bash train_mycobot.sh tqc MyCobotFetchPickAndPlace-Sparse-mocap-v1 --device cuda:1  --hyperparams gradient_steps:1 n_envs:100 learning_starts:10000 &
