2b8cbd631f7c5a10a52ee32f26ad60e660271ae4
Matin Moezzi <matin.moezzi@outlook.com>
2024-02-14 00:29:42 -0500

    Update hyperparameters and training script

--- train_sbx_mycobot_parallel.sh
+++ train_sbx_mycobot_parallel.sh
@@ -1,1 +1,1 @@
-python train_sbx.py --algo $1 --env $2 -P --log-interval 1000 --eval-freq 500000 --save-freq 5000000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track --n-timesteps 15_000_000 --hyperparams gradient_steps:4 n_envs:300 learning_starts:15500 ${@:3}
+python train_sbx.py --algo $1 --env $2 -P --log-interval 1000 --eval-freq 100000 --save-freq 500000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track --n-timesteps 5_000_000 --hyperparams n_envs:200 learning_starts:10500 ${@:3}
