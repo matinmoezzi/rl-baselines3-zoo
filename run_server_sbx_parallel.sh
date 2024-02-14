3d7ff78d982dce272e9cb674a29c98c34d36d076
Matin Moezzi <matin.moezzi@outlook.com>
2024-02-14 13:28:35 -0500

    Update training parameters in run_server_sbx_parallel.sh and train_mycobot_parallel.sh

--- run_server_sbx_parallel.sh
+++ run_server_sbx_parallel.sh
@@ -9,4 +9,4 @@ bash train_sbx_mycobot_parallel.sh tqc MyCobotFetchPickAndPlace-Sparse-mocap-v0 
 
 bash train_sbx_mycobot_parallel.sh tqc MyCobotFetchPickAndPlace-RewardShaping-mocap-v0 --n-timesteps 5_000_000 --device cuda:1 &
 
-bash train_sbx_mycobot_parallel.sh tqc MyCobotFetchPickAndPlace-Sparse-mocap-v1 --n-timesteps 5_000_000 --device cuda:2 &
+bash train_mycobot_parallel.sh tqc MyCobotFetchPickAndPlace-Sparse-mocap-v1 --n-timesteps 5_000_000 --device cuda:2 &
--- train_mycobot_parallel.sh
+++ train_mycobot_parallel.sh
@@ -1,1 +1,1 @@
-python train.py --algo $1 --env $2 -P --log-interval 1000 --eval-freq 500000 --save-freq 5000000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track --hyperparams n_envs:200 learning_starts:10500 ${@:3}
+python train.py --algo $1 --env $2 -P --log-interval 1000 --eval-freq 100000 --save-freq 500000 --wandb-project-name mycobot-deepRL --wandb-entity matinmoezzi --track --hyperparams n_envs:200 learning_starts:10500 ${@:3}
